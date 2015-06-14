453770251390f14cbb3a81420f42e054107b489a
Daniel Goertzen <daniel.goertzen@gmail.com>
2015-06-14 09:15:25 -0500

    improve error reporting, use ESCRIPT env var if present

--- build.rs
+++ build.rs
@@ -8,11 +8,19 @@ use std::env;
 use std::path::Path;
 
 fn main() {
-	// FIXME: take env escript if present
-	let escript = "escript";
+	// use environment escript if available
+	let escript = env::var("ESCRIPT").unwrap_or("escript".to_string());
 
 	// emit generated files to OUT_DIR
-	let out_dir = env::var("OUT_DIR").unwrap();
+	let out_dir = env::var("OUT_DIR")
+		.map_err(|_|"Can't read OUT_DIR env variable.")
+		.unwrap();
+
 	let dst = Path::new(&out_dir);
-	Command::new(escript).arg("gen_api.erl").arg(dst).status().unwrap();
+	match Command::new(escript).arg("gen_api.erl").arg(dst).status()
+		.map_err(|_|"Failed to start gen_api.erl.  Is 'escript' available in the path?")
+		.unwrap().success() {
+			true  => (),
+			false => panic!("gen_api.erl encountered an error.")
+		}
 }
