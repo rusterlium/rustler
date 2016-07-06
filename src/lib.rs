6559066c45878d461ad2e9c24af2055f66a5a00f
Daniel Goertzen <daniel.goertzen@gmail.com>
2016-07-06 15:38:05 -0500

    change ERL_NIF_TERM back to int
    Was previously changed to a tuple struct to increase type strength
    for ruster, but this messed up 32 bit cdecl calling convention.

--- src/lib.rs
+++ src/lib.rs
@@ -124,10 +124,12 @@ use std::option::Option;
 #[allow(non_camel_case_types)]
 pub type ERL_NIF_UINT = size_t;
 
-#[derive(Debug, Copy, Clone)]
-#[repr(C)]
 #[allow(non_camel_case_types)]
-pub struct ERL_NIF_TERM(ERL_NIF_UINT);
+pub type ERL_NIF_TERM = ERL_NIF_UINT;
+
+//#[derive(Debug, Copy, Clone)]
+//#[repr(C)]
+//pub struct ERL_NIF_TERM(ERL_NIF_UINT);  // Don't do this, 32 bin calling convention is different for structs and ints.
 
 
 /// See [ErlNifEnv](http://www.erlang.org/doc/man/erl_nif.html#ErlNifEnv) in the Erlang docs.
