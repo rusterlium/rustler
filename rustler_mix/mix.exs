5bbb41c34336657942277768f51d313c7182b73f
HansiHE <hansihe@hansihe.com>
2016-07-24 13:10:49 +0200

    rustler_mix release

--- rustler_mix/mix.exs
+++ rustler_mix/mix.exs
@@ -3,7 +3,7 @@ defmodule Rustler.Mixfile do
 
   def project do
     [app: :rustler,
-     version: "0.1.1",
+     version: "0.2.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
