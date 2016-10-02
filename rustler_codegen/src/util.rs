ebc6e7775602d99a582e51b2e3c8b0a36744c4c7
Kyle Mayes <kyle@mayeses.com>
2016-10-02 17:47:21 -0400

    Update for latest nightly/syntex (#23)

--- rustler_codegen/src/util.rs
+++ rustler_codegen/src/util.rs
@@ -1,12 +1,11 @@
 use ::syntax::ast::{MetaItem, MetaItemKind};
 use ::syntax::codemap::{Spanned};
-use syntax::attr::AttrMetaMethods;
 
 pub fn get_meta_item_value(meta_item: &MetaItem, name: &str) -> Option<String> {
     match meta_item {
         &Spanned { node: MetaItemKind::List(_, ref items), span: _ } => {
             for ref item in items {
-                if item.name() == name {
+                if item.name().map_or(false, |n| n == name) {
                     match item.value_str() {
                         Some(value) => return Some(value.to_string()),
                         None => (),
