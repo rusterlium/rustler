95fa6696c8fae3aff750cf07aa8a439fd245e265
Hans Elias J <hansihe@hansihe.com>
2016-12-29 19:03:08 +0100

    remove implicit inner RwLock in ResourceCell, require the Sync trait … (#38)
    * remove implicit inner RwLock in ResourceCell, require the Sync trait instead
    
    * store resource contents in box until placement new lands in rustc

--- test/src/test_resource.rs
+++ test/src/test_resource.rs
@@ -1,10 +1,10 @@
 use rustler::NifEncoder;
 use rustler::{NifEnv, NifTerm, NifResult};
 use rustler::resource::ResourceCell;
+use std::sync::RwLock;
 
-//#[NifResource]
 struct TestResource {
-    test_field: i32
+    test_field: RwLock<i32>,
 }
 
 pub fn on_load(env: &NifEnv) -> bool {
@@ -14,7 +14,7 @@ pub fn on_load(env: &NifEnv) -> bool {
 
 pub fn resource_make<'a>(env: &'a NifEnv, _args: &Vec<NifTerm>) -> NifResult<NifTerm<'a>> {
     let data = TestResource {
-        test_field: 0,
+        test_field: RwLock::new(0),
     };
     let resource = ResourceCell::new(data);
 
@@ -23,14 +23,14 @@ pub fn resource_make<'a>(env: &'a NifEnv, _args: &Vec<NifTerm>) -> NifResult<Nif
 
 pub fn resource_set_integer_field<'a>(env: &'a NifEnv, args: &Vec<NifTerm>) -> NifResult<NifTerm<'a>> {
     let resource: ResourceCell<TestResource> = try!(args[0].decode());
-    let mut data = resource.write().unwrap();
-    data.test_field = try!(args[1].decode());
+    let mut test_field = resource.test_field.write().unwrap();
+    *test_field = try!(args[1].decode());
 
     Ok("ok".encode(env))
 }
 
 pub fn resource_get_integer_field<'a>(env: &'a NifEnv, args: &Vec<NifTerm>) ->  NifResult<NifTerm<'a>> {
     let resource: ResourceCell<TestResource> = try!(args[0].decode());
-    let data = resource.read().unwrap();
-    Ok(data.test_field.encode(env))
+    let test_field = resource.test_field.read().unwrap();
+    Ok(test_field.encode(env))
 }
