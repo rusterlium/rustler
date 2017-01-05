22450b8660706820264258e10d026cf0adbeeaf8
Sonny Scroggin <scrogson@gmail.com>
2017-01-05 13:00:11 -0600

    Rename scheduling flags

--- src/schedule.rs
+++ src/schedule.rs
@@ -1,6 +1,12 @@
 use super::NifEnv;
 use super::wrapper::nif_interface::enif_consume_timeslice;
 
+pub enum NifScheduleFlags {
+    NORMAL = 0,
+    DIRTY_CPU = 1,
+    DIRTY_IO = 2,
+}
+
 pub fn consume_timeslice<'a>(env: &'a NifEnv, percent: i32) -> bool {
     let success = unsafe { enif_consume_timeslice(env.as_c_arg(), percent) };
     success == 1
