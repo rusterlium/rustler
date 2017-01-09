dd538337c9caee9c8573a0a10300f154e814b39b
Jason Orendorff <jason.orendorff@gmail.com>
2017-01-09 13:45:41 -0600

    Mark `wrapper::exception` functions `unsafe`.

--- src/wrapper/exception.rs
+++ src/wrapper/exception.rs
@@ -1,10 +1,10 @@
 use super::nif_interface::{ NIF_ENV, NIF_TERM };
 use super::nif_interface;
 
-pub fn raise_exception(env: NIF_ENV, term: NIF_TERM) -> NIF_TERM {
-    unsafe { nif_interface::enif_raise_exception(env, term) }
+pub unsafe fn raise_exception(env: NIF_ENV, term: NIF_TERM) -> NIF_TERM {
+    nif_interface::enif_raise_exception(env, term)
 }
 
-pub fn raise_badarg(env: NIF_ENV) -> NIF_TERM {
-    unsafe { nif_interface::enif_make_badarg(env) }
+pub unsafe fn raise_badarg(env: NIF_ENV) -> NIF_TERM {
+    nif_interface::enif_make_badarg(env)
 }
