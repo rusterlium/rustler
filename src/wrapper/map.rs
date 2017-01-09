6f1f29f37c23da441f9ff85df3b7e406d665b96d
Jason Orendorff <jason.orendorff@gmail.com>
2017-01-09 13:17:40 -0600

    Mark `wrapper::map` functions `unsafe`.

--- src/wrapper/map.rs
+++ src/wrapper/map.rs
@@ -3,9 +3,9 @@ use super::nif_interface::{ NIF_ENV, NIF_TERM, ErlNifMapIteratorEntry };
 pub use super::nif_interface::ErlNifMapIterator;
 use std::mem;
 
-pub fn get_map_value(env: NIF_ENV, map: NIF_TERM, key: NIF_TERM) -> Option<NIF_TERM> {
-    let mut result: NIF_TERM = unsafe { mem::uninitialized() };
-    let success = unsafe { nif_interface::enif_get_map_value(env, map, key, &mut result as *mut NIF_TERM) };
+pub unsafe fn get_map_value(env: NIF_ENV, map: NIF_TERM, key: NIF_TERM) -> Option<NIF_TERM> {
+    let mut result: NIF_TERM = mem::uninitialized();
+    let success = nif_interface::enif_get_map_value(env, map, key, &mut result as *mut NIF_TERM);
 
     if success != 1 {
         return None;
@@ -13,9 +13,9 @@ pub fn get_map_value(env: NIF_ENV, map: NIF_TERM, key: NIF_TERM) -> Option<NIF_T
     Some(result)
 }
 
-pub fn get_map_size(env: NIF_ENV, map: NIF_TERM) -> Option<usize> {
-    let mut size: nif_interface::size_t = unsafe { mem::uninitialized() };
-    let success = unsafe { nif_interface::enif_get_map_size(env, map, &mut size as *mut nif_interface::size_t) };
+pub unsafe fn get_map_size(env: NIF_ENV, map: NIF_TERM) -> Option<usize> {
+    let mut size: nif_interface::size_t = mem::uninitialized();
+    let success = nif_interface::enif_get_map_size(env, map, &mut size as *mut nif_interface::size_t);
 
     if success != 1 {
         return None;
@@ -23,13 +23,13 @@ pub fn get_map_size(env: NIF_ENV, map: NIF_TERM) -> Option<usize> {
     Some(size as usize)
 }
 
-pub fn map_new(env: NIF_ENV) -> NIF_TERM {
-    unsafe { nif_interface::enif_make_new_map(env) }
+pub unsafe fn map_new(env: NIF_ENV) -> NIF_TERM {
+    nif_interface::enif_make_new_map(env)
 }
 
-pub fn map_put(env: NIF_ENV, map: NIF_TERM, key: NIF_TERM, value: NIF_TERM) -> Option<NIF_TERM> {
-    let mut result: NIF_TERM = unsafe { mem::uninitialized() };
-    let success = unsafe { nif_interface::enif_make_map_put(env, map, key, value, &mut result as *mut NIF_TERM) };
+pub unsafe fn map_put(env: NIF_ENV, map: NIF_TERM, key: NIF_TERM, value: NIF_TERM) -> Option<NIF_TERM> {
+    let mut result: NIF_TERM = mem::uninitialized();
+    let success = nif_interface::enif_make_map_put(env, map, key, value, &mut result as *mut NIF_TERM);
 
     if success != 1 {
         return None;
@@ -37,9 +37,9 @@ pub fn map_put(env: NIF_ENV, map: NIF_TERM, key: NIF_TERM, value: NIF_TERM) -> O
     Some(result)
 }
 
-pub fn map_remove(env: NIF_ENV, map: NIF_TERM, key: NIF_TERM) -> Option<NIF_TERM> {
-    let mut result: NIF_TERM = unsafe { mem::uninitialized() };
-    let success = unsafe { nif_interface::enif_make_map_remove(env, map, key, &mut result as *mut NIF_TERM) };
+pub unsafe fn map_remove(env: NIF_ENV, map: NIF_TERM, key: NIF_TERM) -> Option<NIF_TERM> {
+    let mut result: NIF_TERM = mem::uninitialized();
+    let success = nif_interface::enif_make_map_remove(env, map, key, &mut result as *mut NIF_TERM);
 
     if success != 1 {
         return None;
@@ -47,9 +47,9 @@ pub fn map_remove(env: NIF_ENV, map: NIF_TERM, key: NIF_TERM) -> Option<NIF_TERM
     Some(result)
 }
 
-pub fn map_update(env: NIF_ENV, map: NIF_TERM, key: NIF_TERM, new_value: NIF_TERM) -> Option<NIF_TERM> {
-    let mut result: NIF_TERM = unsafe { mem::uninitialized() };
-    let success = unsafe { nif_interface::enif_make_map_update(env, map, key, new_value, &mut result as *mut NIF_TERM) };
+pub unsafe fn map_update(env: NIF_ENV, map: NIF_TERM, key: NIF_TERM, new_value: NIF_TERM) -> Option<NIF_TERM> {
+    let mut result: NIF_TERM = mem::uninitialized();
+    let success = nif_interface::enif_make_map_update(env, map, key, new_value, &mut result as *mut NIF_TERM);
 
     if success != 1 {
         return None;
--- src/wrapper/mod.rs
+++ src/wrapper/mod.rs
@@ -14,7 +14,6 @@ pub mod tuple;
 pub use self::tuple::{ get_tuple };
 
 pub mod map;
-pub use self::map::{ get_map_value, get_map_size, map_new, map_put };
 
 pub mod atom;
 
