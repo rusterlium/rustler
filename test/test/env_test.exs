46bf9fec6f6c7af5f1cb439c5d07c93aa094a74b
Jason Orendorff <jason.orendorff@gmail.com>
2017-01-09 15:27:37 -0600

    `env.send(&pid, message)`.

--- test/test/env_test.exs
+++ test/test/env_test.exs
@@ -1,13 +1,44 @@
 defmodule RustlerTest.EnvTest do
   use ExUnit.Case, async: true
 
+  test "send" do
+    # Start 4 processes. Each one waits for one message,
+    # then sends a tuple back to this process.
+    parent = self()
+    child_pids = for _ <- 1..4 do
+      spawn(
+        fn ->
+          receive do
+            # Send the message itself, plus this process's pid.
+            msg -> send(parent, {self(), msg})
+          end
+        end)
+    end
+
+    # Send all 4 child processes a message.
+    RustlerTest.send_all(child_pids, {:hello, :world})
+
+    # Wait for all 4 processes to send something back.
+    results = for _ <- 1..4 do
+      receive do
+        # Collect the pids that the child processes send back.
+        {child_pid, {:hello, :world}} -> child_pid
+        _ -> raise :fail
+      end
+    end
+
+    # Each child should have sent back one result. (Sort before testing
+    # because results come back in arbitrary order.)
+    assert Enum.sort(results) == Enum.sort(child_pids)
+  end
+
   test "owned environments" do
     shop_menu = [
       {:spaniel, 34},
       {:ring_of_power, 75},
       {:rope, 4}
     ]
-    RustlerTest.sublists shop_menu
+    RustlerTest.sublists(shop_menu)
     receive do
       x ->
         assert x == [
