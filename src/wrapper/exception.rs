78dbbba84bc26cf9e6bdf66799c50fb78bd76dd7
Jason Orendorff <jason.orendorff@gmail.com>
2017-01-12 17:21:32 -0600

    Document the real reason exception-related functions are unsafe.

--- src/wrapper/exception.rs
+++ src/wrapper/exception.rs
@@ -1,10 +1,28 @@
 use super::nif_interface::{ NIF_ENV, NIF_TERM };
 use super::nif_interface;
 
+/// Raise an "error exception".
+///
+/// # Unsafe
+///
+/// The value returned by this function "can be used only as the return value
+/// from the NIF that invoked it (directly or indirectly) or be passed to
+/// `enif_is_exception`, but not to any other NIF API function."
+///
+/// And of course the usual rules about `env` and `term` still apply.
 pub unsafe fn raise_exception(env: NIF_ENV, term: NIF_TERM) -> NIF_TERM {
     nif_interface::enif_raise_exception(env, term)
 }
 
+/// Raise a `badarg` exception.
+///
+/// # Unsafe
+///
+/// The value returned by this function "can be used only as the return value
+/// from the NIF that invoked it (directly or indirectly) or be passed to
+/// `enif_is_exception`, but not to any other NIF API function."
+///
+/// And of course `env` must be a valid environment.
 pub unsafe fn raise_badarg(env: NIF_ENV) -> NIF_TERM {
     nif_interface::enif_make_badarg(env)
 }
