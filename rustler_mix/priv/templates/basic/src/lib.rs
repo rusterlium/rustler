c6c40d1d4211f30970fb43efa828b13b85f561b9
Hans Elias J <hansihe@hansihe.com>
2017-02-03 00:39:21 +0100

    switch over to rust 1.15 proc macros for rustler_codegen (#81)

--- rustler_mix/priv/templates/basic/src/lib.rs
+++ rustler_mix/priv/templates/basic/src/lib.rs
@@ -1,17 +1,27 @@
-// When writing your NIF you should edit `lib.in.rs`. This is done
-// to support both compilation with and without syntex.
+#[macro_use] extern crate rustler;
+#[macro_use] extern crate rustler_codegen;
+#[macro_use] extern crate lazy_static;
 
-// Enable the rustler_codegen compiler plugin if we are not using
-// syntex.
-#![cfg_attr(not(feature = "with-syntex"), feature(plugin))]
-#![cfg_attr(not(feature = "with-syntex"), plugin(rustler_codegen))]
+use rustler::{NifEnv, NifTerm, NifResult, NifEncoder};
 
-// If we are using syntex, include the expanded file. This is what
-// causes bad error messages.
-#[cfg(feature = "with-syntex")]
-include!(concat!(env!("OUT_DIR"), "/lib.rs"));
+mod atoms {
+    rustler_atoms! {
+        atom ok;
+        //atom error;
+        //atom __true__ = "true";
+        //atom __false__ = "false";
+    }
+}
 
-// If we are on nightly, we include the file directly.
-// The rust compiler will then report errors in that file.
-#[cfg(not(feature = "with-syntex"))]
-include!("lib.in.rs");
+rustler_export_nifs! {
+    "<%= native_module %>",
+    [("add", 2, add)],
+    None
+}
+
+fn add<'a>(env: NifEnv<'a>, args: &[NifTerm<'a>]) -> NifResult<NifTerm<'a>> {
+    let num1: i64 = try!(args[0].decode());
+    let num2: i64 = try!(args[1].decode());
+
+    Ok((atoms::ok(), num1 + num2).encode(env))
+}
--- test/src/lib.rs
+++ test/src/lib.rs
@@ -1,17 +1,63 @@
-// Enable the rustler_codegen compiler plugin if we are not using
-// syntex.
-#![cfg_attr(not(feature = "with-syntex"), feature(plugin))]
-#![cfg_attr(not(feature = "with-syntex"), plugin(rustler_codegen))]
+#[macro_use]
+extern crate rustler;
+#[macro_use]
+extern crate rustler_codegen;
 
-#[macro_use] extern crate rustler;
-#[macro_use] extern crate lazy_static;
+#[macro_use]
+extern crate lazy_static;
 
-// If we are using syntex, include the expanded file. This is what
-// causes bad error messages.
-#[cfg(feature = "with-syntex")]
-include!(concat!(env!("OUT_DIR"), "/lib.rs"));
+use rustler::{NifEnv, NifTerm};
 
-// If we are on nightly, we include the file directly.
-// The rust compiler will then report errors in that file.
-#[cfg(not(feature = "with-syntex"))]
-include!("lib.in.rs");
+mod test_primitives;
+mod test_list;
+mod test_map;
+mod test_resource;
+mod test_binary;
+mod test_atom;
+mod test_thread;
+mod test_env;
+
+rustler_export_nifs!(
+    "Elixir.RustlerTest",
+    [
+        ("add_u32", 2, test_primitives::add_u32),
+        ("add_i32", 2, test_primitives::add_i32),
+        ("echo_u8", 1, test_primitives::echo_u8),
+        ("tuple_add", 1, test_primitives::tuple_add),
+
+        ("sum_list", 1, test_list::sum_list),
+        ("make_list", 0, test_list::make_list),
+
+        ("sum_map_values", 1, test_map::sum_map_values),
+        ("map_entries_sorted", 1, test_map::map_entries_sorted),
+
+        ("resource_make", 0, test_resource::resource_make),
+        ("resource_set_integer_field", 2, test_resource::resource_set_integer_field),
+        ("resource_get_integer_field", 1, test_resource::resource_get_integer_field),
+        ("resource_make_immutable", 1, test_resource::resource_make_immutable),
+        ("resource_immutable_count", 0, test_resource::resource_immutable_count),
+
+        ("atom_to_string", 1, test_atom::atom_to_string),
+        ("atom_equals_ok", 1, test_atom::atom_equals_ok),
+
+        ("make_shorter_subbinary", 1, test_binary::make_shorter_subbinary),
+        ("parse_integer", 1, test_binary::parse_integer),
+        ("binary_new", 0, test_binary::binary_new),
+        ("unowned_to_owned", 1, test_binary::unowned_to_owned),
+        ("realloc_shrink", 0, test_binary::realloc_shrink),
+        ("realloc_grow", 0, test_binary::realloc_grow),
+
+        ("threaded_fac", 1, test_thread::threaded_fac),
+        ("threaded_sleep", 1, test_thread::threaded_sleep),
+
+        ("send_all", 2, test_env::send_all),
+        ("sublists", 1, test_env::sublists),
+    ],
+    Some(on_load)
+);
+
+fn on_load<'a>(env: NifEnv<'a>, _load_info: NifTerm<'a>) -> bool {
+    test_resource::on_load(env);
+    test_atom::on_load(env);
+    true
+}
