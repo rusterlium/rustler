783a97290f635d74311b36b83d7dee68dd95bce8
Alex Shapiro <alexander.max.shapiro@gmail.com>
2017-05-20 15:32:48 -0700

    implement NifEncoder for Option<T> and Result<T,E> (#94)

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -39,3 +39,21 @@ impl<'a, T> NifEncoder for &'a T where T: NifEncoder {
         <T as NifEncoder>::encode(self, env)
     }
 }
+
+impl<T> NifEncoder for Option<T> where T: NifEncoder {
+    fn encode<'c>(&self, env: NifEnv<'c>) -> NifTerm<'c> {
+        match *self {
+            Some(ref value) => value.encode(env),
+            None => atom::nil().encode(env),
+        }
+    }
+}
+
+impl<T, E> NifEncoder for Result<T, E> where T: NifEncoder, E: NifEncoder {
+    fn encode<'c>(&self, env: NifEnv<'c>) -> NifTerm<'c> {
+        match *self {
+            Ok(ref value) => (atom::ok().encode(env), value.encode(env)).encode(env),
+            Err(ref err) => (atom::error().encode(env), err.encode(env)).encode(env),
+        }
+    }
+}
