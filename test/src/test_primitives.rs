b3c76f4bc903399bba899077e9f571156366a7a6
Alex Shapiro <alexander.max.shapiro@gmail.com>
2017-05-23 06:14:34 -0700

    implement NifDecoder for Option<T> and Result<T,E> (#96)
    * implement NifEncoder for Option<T> and Result<T,E>
    
    * implement NifDecoder for Option<T> and Result<T,E>
    
    Also adds tests for encoding + decoding Option<T> and Result<T,E> types.

--- test/src/test_primitives.rs
+++ test/src/test_primitives.rs
@@ -18,3 +18,19 @@ pub fn echo_u8<'a>(env: NifEnv<'a>, args: &[NifTerm<'a>]) -> NifResult<NifTerm<'
     let num: u8 = try!(args[0].decode());
     Ok(num.encode(env))
 }
+
+pub fn option_inc<'a>(env: NifEnv<'a>, args: &[NifTerm<'a>]) -> NifResult<NifTerm<'a>> {
+    let opt: Option<f64> = args[0].decode()?;
+    let incremented = opt.and_then(|num| Some(num + 1.0));
+    Ok(incremented.encode(env))
+}
+
+pub fn result_to_int<'a>(env: NifEnv<'a>, args: &[NifTerm<'a>]) -> NifResult<NifTerm<'a>> {
+    let result: Result<bool, &'a str> = args[0].decode()?;
+    let int_result = match result {
+        Ok(true) => Ok(1),
+        Ok(false) => Ok(0),
+        Err(errstr) => Err(format!("{}{}", errstr, errstr)),
+    };
+    Ok(int_result.encode(env))
+}
