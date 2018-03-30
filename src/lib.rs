a3a0b91466226f603d3906bfb41d2d42a7117c50
Sonny Scroggin <sonny@scrogg.in>
2018-03-30 10:00:08 -0500

    Move Env and Error to modules (#136)

--- src/lib.rs
+++ src/lib.rs
@@ -25,10 +25,7 @@
 #[macro_use(enif_snprintf)]
 extern crate erlang_nif_sys;
 
-use std::marker::PhantomData;
-
 mod wrapper;
-use wrapper::nif_interface::NIF_ENV;
 
 #[doc(hidden)]
 pub mod codegen_runtime;
@@ -41,8 +38,8 @@ pub mod types;
 
 mod term;
 
-pub use term::{ Term };
-pub use types::{Encoder, Decoder};
+pub use term::Term;
+pub use types::{Decoder, Encoder};
 pub mod resource;
 
 #[doc(hidden)]
@@ -50,116 +47,13 @@ pub mod dynamic;
 pub use dynamic::TermType;
 
 pub mod schedule;
+pub use schedule::SchedulerFlags;
 pub mod env;
+pub use env::Env;
 pub mod thread;
 
 mod export;
+pub mod error;
+pub use error::Error;
 
 pub type NifResult<T> = Result<T, Error>;
-
-pub use schedule::SchedulerFlags;
-
-/// Private type system hack to help ensure that each environment exposed to safe Rust code is
-/// given a different lifetime. The size of this type is zero, so it costs nothing at run time. Its
-/// purpose is to make `Env<'a>` and `Term<'a>` *invariant* w.r.t. `'a`, so that Rust won't
-/// auto-convert a `Env<'a>` to a `Env<'b>`.
-type EnvId<'a> = PhantomData<*mut &'a u8>;
-
-/// On each NIF call, a Env is passed in. The Env is used for most operations that involve
-/// communicating with the BEAM, like decoding and encoding terms.
-///
-/// There is no way to allocate a Env at the moment, but this may be possible in the future.
-#[derive(Clone, Copy)]
-pub struct Env<'a> {
-    env: NIF_ENV,
-    id: EnvId<'a>
-}
-
-/// Two environments are equal if they're the same `NIF_ENV` value.
-///
-/// A `Env<'a>` is equal to a `Env<'b>` iff `'a` and `'b` are the same lifetime.
-impl<'a, 'b> PartialEq<Env<'b>> for Env<'a> {
-    fn eq(&self, other: &Env<'b>) -> bool {
-        self.env == other.env
-    }
-}
-
-impl<'a> Env<'a> {
-    /// Create a new Env. For the `_lifetime_marker` argument, pass a
-    /// reference to any local variable that has its own lifetime, different
-    /// from all other `Env` values. The purpose of the argument is to make
-    /// it easier to know for sure that the `Env` you're creating has a
-    /// unique lifetime (i.e. that you're following the most important safety
-    /// rule of Rustler).
-    ///
-    /// # Unsafe
-    /// Don't create multiple `Env`s with the same lifetime.
-    unsafe fn new<T>(_lifetime_marker: &'a T, env: NIF_ENV) -> Env<'a> {
-        Env {
-            env: env,
-            id: PhantomData
-        }
-    }
-
-    pub fn as_c_arg(&self) -> NIF_ENV {
-        self.env
-    }
-
-    /// Convenience method for building a tuple `{error, Reason}`.
-    pub fn error_tuple<T>(self, reason: T) -> Term<'a>
-        where T: Encoder
-    {
-        let error = types::atom::error().to_term(self);
-        (error, reason).encode(self)
-    }
-}
-
-/// Represents usual errors that can happen in a nif. This enables you
-/// to return an error from anywhere, even places where you don't have
-/// an Env availible.
-pub enum Error {
-    /// Returned when the NIF has been called with the wrong number or type of
-    /// arguments.
-    BadArg,
-    /// Encodes the string into an atom and returns it from the NIF.
-    Atom(&'static str),
-    RaiseAtom(&'static str),
-    RaiseTerm(Box<Encoder>),
-}
-
-impl Error {
-
-    /// # Unsafe
-    ///
-    /// If `self` is a `BadArg`, `RaiseAtom`, or `RaiseTerm` value, then the
-    /// term returned from this method must not be used except as the return
-    /// value from the calling NIF.
-    unsafe fn encode<'a>(self, env: Env<'a>) -> Term<'a> {
-        match self {
-            Error::BadArg => {
-                let exception = wrapper::exception::raise_badarg(env.as_c_arg());
-                Term::new(env, exception)
-            },
-            Error::Atom(atom_str) => {
-                types::atom::Atom::from_str(env, atom_str)
-                    .ok().expect("Error::Atom: bad atom").to_term(env)
-            },
-            Error::RaiseAtom(atom_str) => {
-                let atom = types::atom::Atom::from_str(env, atom_str)
-                    .ok().expect("Error::RaiseAtom: bad argument");
-                let exception = wrapper::exception::raise_exception(
-                    env.as_c_arg(),
-                    atom.as_c_arg());
-                Term::new(env, exception)
-            },
-            Error::RaiseTerm(ref term_unencoded) => {
-                let term = term_unencoded.encode(env);
-                let exception = wrapper::exception::raise_exception(
-                    env.as_c_arg(),
-                    term.as_c_arg());
-                Term::new(env, exception)
-            },
-        }
-    }
-
-}
