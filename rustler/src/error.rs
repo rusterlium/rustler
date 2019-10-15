8d6613305dc8b7300d63db28e6c4117ce6c54df5
Benedikt Reinartz <filmor@gmail.com>
2019-10-15 08:47:23 +0100

    Add a simple Debug impl for Error

--- rustler/src/error.rs
+++ rustler/src/error.rs
@@ -1,9 +1,10 @@
 use crate::codegen_runtime::{NifReturnable, NifReturned};
 use crate::{types, Encoder, Env};
+use std::fmt;
 
 /// Represents usual errors that can happen in a nif. This enables you
 /// to return an error from anywhere, even places where you don't have
-/// an Env availible.
+/// an Env available.
 pub enum Error {
     /// Returned when the NIF has been called with the wrong number or type of
     /// arguments.
@@ -35,6 +36,17 @@ unsafe impl NifReturnable for crate::error::Error {
                 let term = term_unencoded.encode(env);
                 NifReturned::Raise(term.as_c_arg())
             }
+        }
+    }
+}
+
+impl fmt::Debug for Error {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match self {
+            Error::BadArg => write!(fmt, "{{error, badarg}}"),
+            Error::Atom(ref s) => write!(fmt, "{{error, {}}}", s),
+            Error::RaiseAtom(ref s) => write!(fmt, "throw({})", s),
+            Error::RaiseTerm(_) => write!(fmt, "throw(<term>)"),
         }
     }
 }
