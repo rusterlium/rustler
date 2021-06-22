aeea30802dc3e710bdb0b0231e5b88bd2895b3a7
Magnus Ottenklinger <evnu@posteo.de>
2021-06-22 10:13:15 +0200

    Set @version in Mixfile on release

--- release.sh
+++ release.sh
@@ -44,6 +44,7 @@ sed -i "s/^version = \"[^\"]*\" # rustler version$/version = \"$VERSION\" # rust
 sed -i "s/^rustler_codegen.*$/rustler_codegen = { path = \"..\/rustler_codegen\", version = \"$VERSION\", optional = true}/" rustler/Cargo.toml
 sed -i "s/^version = \"[^\"]*\" # rustler_codegen version$/version = \"$VERSION\" # rustler_codegen version/" rustler_codegen/Cargo.toml
 sed -i "s/def rustler_version, do: \"[^\"]*\"$/def rustler_version, do: \"$VERSION\"/" rustler_mix/mix.exs rustler_mix/lib/rustler.ex
+sed -i "s/@version .*$/@version \"$VERSION\"/" rustler_mix/mix.exs
 sed -i "s/{:rustler, \".*\"}/{:rustler, \"~> $VERSION\"}/" rustler_mix/README.md
 
 echo "Committing version.."
