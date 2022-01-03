4d8a93ecf6097e3aab60982046b063448584a45b
Magnus <evnu@posteo.de>
2022-01-03 13:26:53 +0100

    Bump heck to 0.4 (#421)

--- rustler_codegen/src/unit_enum.rs
+++ rustler_codegen/src/unit_enum.rs
@@ -1,7 +1,7 @@
 use proc_macro2::{Span, TokenStream};
 use quote::{quote, quote_spanned};
 
-use heck::SnakeCase;
+use heck::ToSnakeCase;
 use syn::{self, spanned::Spanned, Fields, Ident, Variant};
 
 use super::context::Context;
