7869fb00f240b14a6cdea40c26a2c26ebe256e16
Benedikt Reinartz <filmor@gmail.com>
2023-05-18 13:58:50 +0200

    Make rustler_sys forward compatible (#536)

--- rustler_sys/src/rustler_sys_api.rs
+++ rustler_sys/src/rustler_sys_api.rs
@@ -187,6 +187,7 @@ pub enum ErlNifResourceFlags {
 #[repr(C)]
 pub enum ErlNifCharEncoding {
     ERL_NIF_LATIN1 = 1,
+    #[cfg(nif_2_17)]
     ERL_NIF_UTF8 = 2,
 }
 
@@ -330,6 +331,7 @@ pub enum ErlNifTermType {
 }
 
 /// See [ErlNifOption](http://www.erlang.org/doc/man/erl_nif.html#ErlNifOption) in the Erlang docs.
+#[cfg(nif_2_17)]
 #[derive(Copy, Clone)]
 #[repr(C)]
 pub enum ErlNifOption {
