69053dfbab639197924d960cd5516c8494137e67
Cocoa <i@uwucocoa.moe>
2024-06-27 22:57:08 +0100

    make `align_alloced_mem_for_struct` public

--- rustler/src/resource.rs
+++ rustler/src/resource.rs
@@ -109,7 +109,7 @@ fn get_alloc_size_struct<T>() -> usize {
 /// Given a pointer `ptr` to an allocation of `get_alloc_size_struct::<T>()` bytes, return the
 /// first aligned pointer within the allocation where a `T` may be stored.
 /// Unsafe: `ptr` must point to a large enough allocation and not be null.
-unsafe fn align_alloced_mem_for_struct<T>(ptr: *const c_void) -> *const c_void {
+pub unsafe fn align_alloced_mem_for_struct<T>(ptr: *const c_void) -> *const c_void {
     let offset = mem::align_of::<T>() - ((ptr as usize) % mem::align_of::<T>());
     ptr.add(offset)
 }
