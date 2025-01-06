021caf79e59920f5399673ecb6bb24f72710e328
Benedikt Reinartz <filmor@gmail.com>
2025-01-06 09:40:17 +0100

    Current state

--- rustler/src/wrapper/tuple.rs
+++ rustler/src/wrapper/tuple.rs
@@ -1,6 +1,5 @@
 use crate::sys::enif_make_tuple_from_array;
-use crate::wrapper::{c_int, NIF_ENV, NIF_ERROR, NIF_TERM};
-use std::mem::MaybeUninit;
+use crate::wrapper::{NIF_ENV, NIF_TERM};
 
 pub unsafe fn make_tuple(env: NIF_ENV, terms: &[NIF_TERM]) -> NIF_TERM {
     enif_make_tuple_from_array(env, terms.as_ptr(), terms.len() as u32)
